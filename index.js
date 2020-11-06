f0ec312794db50e7106ff65f7cabf7f427431ade
Christian Fei <crifei93@gmail.com>
2020-11-06 18:59:09 +0100

    parse url and link

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+#!/usr/bin/env node
+
 const getHTML = require('./lib/get-html')
 const parseNews = require('./lib/parse-news')
 
-module.exports = async function getNews () {
+if (require.main === module) {
+  getNews()
+    .then(news => {
+      console.log(news)
+      for (const n of news) {
+        process.stdout.write(`${n.title} - ${n.upvotes} ${n.comments}\n`)
+      }
+    })
+    .catch(console.error)
+} else {
+  module.exports = getNews
+}
+
+async function getNews () {
   const html = await getHTML()
   return parseNews(html)
 }
