0fe3d7271c2fec61d05a747e3f4b573ee467ea2c
Christian Fei <crifei93@gmail.com>
2020-11-06 16:43:25 +0100

    test getHTML

--- index.test.js
+++ index.test.js
@@ -1,4 +1,12 @@
 const test = require('ava')
+const got = require('got')
 
 test('gets html from https://news.ycombinator.com/', async t => {
+  const html = await getHTML()
+  t.true(typeof html === 'string')
 })
+
+async function getHTML () {
+  return got('https://news.ycombinator.com')
+    .then(res => res.body)
+}
